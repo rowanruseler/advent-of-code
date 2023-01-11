7589b175bbf53de37b73bfb8b6cebb36fb5907c5
Rowan Ruseler <rowanruseler@gmail.com>
2023-01-11 20:22:36 +0100

    next digit and halfway around answer, day 1
    Signed-off-by: Rowan Ruseler <rowanruseler@gmail.com>

--- 2017/day/1/inverse-captcha.c
+++ 2017/day/1/inverse-captcha.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void reverse_captcha(int *captcha, int size) {
+int reverse_captcha(int *captcha, int size, int offset)
+{
     int answer = 0;
-    for (int x = 0; x < size; x++) {
-        if (captcha[x] == captcha[(x + 1) % size]) {
-            answer += captcha[x];
+    for (int i = 0; i < size; i++)
+    {
+        if (captcha[i] == captcha[(i + offset) % size])
+        {
+            answer += captcha[i];
         }
     }
-    printf("answer: %d\n", answer);
+    return answer;
 }
 
-int main() {
-    int captcha[] = {1,1,2,2};
-    int size = (sizeof(captcha)/sizeof(int));
-    reverse_captcha(captcha, size);
+int main()
+{
+    int c;
+    int size = 0; // 2125
+    FILE *input = fopen("input.txt", "r");
+    if (! input)
+    {
+        return 1;
+    }
+    do
+    {
+        c = fgetc(input);
+        if (c == '\n')
+        {
+            break;
+        }
+        size++;
+    } while(1);
+    rewind(input);
+
+    int captcha[size];
+    for (int i = 0; i < size; i++)
+    {
+        c = fgetc(input);
+        // ascii 48 equals 0, so if printf decimal of ascii c we will get
+        // ascii 49, so if we do 49 - 48, we get the decimal 1.
+        captcha[i] = (c - '0');
+    }
+    fclose(input);
+
+    printf("next digit answer: %d\n", reverse_captcha(captcha, size, 1));
+    printf("halfway around answer: %d\n", reverse_captcha(captcha, size, (size/2)));
 }
