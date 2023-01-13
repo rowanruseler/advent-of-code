0a2e56e0ca313bebf6d72d559521edaa16d9e7f7
Rowan Ruseler <rowanruseler@gmail.com>
2023-01-13 13:38:17 +0100

    2017 day 2 rewrote a function return
    Signed-off-by: Rowan Ruseler <rowanruseler@gmail.com>

--- 2017/day/2/checksum.c
+++ 2017/day/2/checksum.c
@@ -3,19 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
- * Function: checksum
- * ------------------
- *  iterates over the array to determine the difference between the largest
- *  value and the smallest value.
- *
- *  *numbers: a pointer to an array of numbers
- *  size: the size of the array
- *
- *  returns: the difference between the largest numbers and smallest numbers
- *           by substracting the smallest number over the largest number
- *
- */
 int checksum(int *numbers, int size)
 {
         int min = INT_MAX;
@@ -30,19 +17,6 @@ int checksum(int *numbers, int size)
         return (max - min);
 }
 
-/*
- * Function: divisible
- * -------------------
- *  iterates twice over the array to find the only two numbers where one
- *  evenly divides the other - that is, where the result of the division
- *  operation is a whole number.
- *
- *  *numbers: a pointer to an array of numbers
- *  size: the size of the array
- *
- *  returns: the division of the divisors by dividing the two numbers that
- *           are divisors
- */
 int divisible(int *numbers, int size)
 {
     int number1;
@@ -55,44 +29,35 @@ int divisible(int *numbers, int size)
             number2 = numbers[j];
             if (number1 > number2) {
                 if (number1 % number2 == 0 || number2 % number1 == 0) {
-                    divisor = (number1 / number2);
+                    return (number1 / number2);
                 }
             }
         }
     }
-
-    return (divisor);
 }
 
-/*
- * Function: main
- * --------------
- *  loops over each line from input file "input.txt" to create an array of numbers
- *  without any tokens to then be used by two different functions
- */
 int main()
 {
     FILE *stream;
     stream = fopen("input.txt", "r");
     if (stream == NULL) {
         perror("fopen");
-        exit(EXIT_FAILURE); // ref: man 3 exit
+        exit(EXIT_FAILURE);
     }
 
     int size = 0;
     int sum = 0;
     int divisor = 0;
     char *line = NULL;
-    size_t len = 0; // ref: man 3 size_t
+    size_t len = 0;
 
-    // ref: man 3 getline
     while (getline(&line, &len, stream) != -1) {
         int numbers[16];
         int i = 0;
-        char *number = strtok(line, "\t"); // ref: man 3 strtok
+        char *number = strtok(line, "\t");
 
         while (number != NULL) {
-            numbers[i++] = atoi(number); // ref: man 3 atoi
+            numbers[i++] = atoi(number);
             number = strtok(NULL, "\t");
         }
 
@@ -103,7 +68,7 @@ int main()
 
     printf("checksum answer: %d, divisible answer: %d\n", sum, divisor);
 
-    free(line); // ref: man 3 free
+    free(line);
     fclose(stream);
     exit(EXIT_SUCCESS);
 }
