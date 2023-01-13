675e7c1548381599f2e9037f7e089412f925527c
Rowan Ruseler <rowanruseler@gmail.com>
2023-01-13 13:31:44 +0100

    rewrote 2017 day 1
    Signed-off-by: Rowan Ruseler <rowanruseler@gmail.com>

--- 2017/day/1/inverse-captcha.c
+++ 2017/day/1/inverse-captcha.c
@@ -1,48 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int reverse_captcha(int *captcha, int size, int offset)
+int reverse_captcha(char *captcha, int len, int offset)
 {
     int answer = 0;
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < len; i++)
     {
-        if (captcha[i] == captcha[(i + offset) % size])
+        if (captcha[i] == captcha[(i + offset) % len])
         {
-            answer += captcha[i];
+            answer += captcha[i] - '0';
         }
     }
+
     return answer;
 }
 
 int main()
 {
-    int c;
-    int size = 0; // 2125
-    FILE *input = fopen("input.txt", "r");
-    if (! input)
-    {
-        return 1;
+    FILE *stream;
+    stream = fopen("input.txt", "r");
+    if (stream == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
     }
-    do
-    {
-        c = fgetc(input);
-        if (c == '\n')
-        {
-            break;
-        }
-        size++;
-    } while(1);
-    rewind(input);
 
-    int captcha[size];
-    for (int i = 0; i < size; i++)
-    {
-        c = fgetc(input);
-        // ascii 48 equals 0, so if printf decimal of ascii c we will get
-        // ascii 49, so if we do 49 - 48, we get the decimal 1.
-        captcha[i] = (c - '0');
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, stream) != -1) {
+        line[strcspn(line, "\n")] = 0;
+        len = strlen(line);
+        printf("next digit answer: %d\n", reverse_captcha(line, len, 1));
+        printf("halfway around answer: %d\n", reverse_captcha(line, len, (len/2)));
     }
-    fclose(input);
 
-    printf("next digit answer: %d\n", reverse_captcha(captcha, size, 1));
-    printf("halfway around answer: %d\n", reverse_captcha(captcha, size, (size/2)));
+    free(line);
+    fclose(stream);
+    exit(EXIT_SUCCESS);
 }
